e976f2fe7b28473082bea16517d52d802804748d
lynxwint3r <dufour59.bastien@gmail.com>
2022-09-27 10:46:30 +0200

    00898-easy-includes done

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,10 @@
-type Includes<T extends readonly any[], U> = any
+// type Includes<T extends readonly unknown[], U> = U extends T[number] ? true : false
+
+// import type { Equal } from '@type-challenges/utils'
+
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
+  ? (<T>() => T extends U ? 1 : 2) extends (<T>() => T extends First ? 1 : 2)
+    ? true
+    : Includes<Rest, U>
+  : false
+
